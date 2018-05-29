7ac3f4510e9e5f94dc896d181c89e33c73ac5e71
ThibaudMZN <thibaud.marazano@gmail.com>
2018-05-29 11:25:26 +0200

    First clock and reset test

--- include/port.h
+++ include/port.h
@@ -16,18 +16,29 @@
  template<const int n = 32>
  SC_MODULE(port_n)                      // declare port sc_module
  {
-   // sc_in<sc_uint<n> > A, B;          // input signal ports
-   // sc_out<sc_uint<n+1> > F;          // output signal ports
+   sc_in<bool> clk, rst;
+   sc_out<int> gpio;                    // int is 32 bits
 
    void do_update_port()
    {
+     if(clk) {
+       if(rst) {
+
+       } else {
+
+       }
+     }
      //F.write(A.read() + B.read());
      // TODO : describe internal register behavior
+   }
+
+   void do_reset(){
+     
    }
 
    SC_CTOR(port_n)                      // constructor for port_n
    {
      SC_METHOD(do_update_port);         // register do_update_port with kernel
-     //sensitive << A << B;               // sensitivity list
+     sensitive << clk << rst;               // sensitivity list
    }
  };
