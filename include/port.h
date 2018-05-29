bd114872cd5b72b51e1d6b9d28c269dd3524b6f6
ThibaudMZN <thibaud.marazano@gmail.com>
2018-05-29 13:03:14 +0200

    Handle Base Addr. in port.h

--- include/port.h
+++ include/port.h
@@ -13,11 +13,12 @@
 
  #include "systemc.h"
 
- template<const int n = 32>
+ template<const int n = 32, const int addr = 0>
  SC_MODULE(port_n)                      // declare port sc_module
  {
    sc_in<bool> clk, rst;
    sc_out<int> gpio;                    // int is 32 bits
+   int base_addr;
 
    void do_update_port()
    {
@@ -25,7 +26,7 @@
        if(rst) {
 
        } else {
-
+         gpio.write(base_addr);
        }
      }
      //F.write(A.read() + B.read());
@@ -33,12 +34,13 @@
    }
 
    void do_reset(){
-     
+
    }
 
    SC_CTOR(port_n)                      // constructor for port_n
    {
      SC_METHOD(do_update_port);         // register do_update_port with kernel
      sensitive << clk << rst;               // sensitivity list
+     base_addr = addr;
    }
  };
