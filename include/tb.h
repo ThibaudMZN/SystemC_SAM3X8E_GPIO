6a47dab90239ec31644883dd1a543cf022024eb5
ThibaudMZN <thibaud.marazano@gmail.com>
2018-05-29 11:19:23 +0200

    Adding initial tesbench

---
+++ include/tb.h
@@ -0,0 +1,38 @@
+/**
+ *  @file    tb.h
+ *  @author Pacôme Lebeau (Pakish)
+ *  @date    29/05/2018
+ *  @version 1.0
+ *
+ *  @section DESCRIPTION
+ *
+ *  TestBench for main.cpp, containing simulation
+ *
+ */
+
+ #include "systemc.h"
+ #include <iostream>
+
+ using namespace std;
+
+ template<const int n = 8>
+ SC_MODULE(stim)
+ {
+   sc_out<bool> clk, rst;
+
+   void StimGen()
+   {
+     clk.write(false);
+     rst.write(false);
+
+     cout << "Done" << endl;
+
+     sc_stop();
+   }
+
+   SC_CTOR(stim)
+   {
+     SC_THREAD(StimGen);
+   }
+
+ };
