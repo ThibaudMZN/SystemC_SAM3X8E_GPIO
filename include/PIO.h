e8751e96afee795abf793daa942eb06635af409c
MaximeFouillat <31452339+MaximeFouillat@users.noreply.github.com>
2018-05-29 16:46:27 +0200

    add Output register in PIO.h and PIO.cpp

--- include/PIO.h
+++ include/PIO.h
@@ -22,11 +22,17 @@ using namespace std;
 
  public:
  	 PIO(uint32_t base);
+   // Pin
    Register PIO_PER = Register(base_address, PIO_PER_OFFSET, 0);
    Register PIO_PDR = Register(base_address, PIO_PDR_OFFSET, 0);
    Register PIO_PSR = Register(base_address, PIO_PSR_OFFSET, 0);
+   // Pull Up
    Register PIO_PUER = Register(base_address, PIO_PUER_OFFSET, 0);
    Register PIO_PUDR = Register(base_address, PIO_PUDR_OFFSET, 0);
    Register PIO_PUSR = Register(base_address, PIO_PUSR_OFFSET, 0);
+   // Output
+   Register PIO_OER = Register(base_address, PIO_OER_OFFSET, 0);
+   Register PIO_ODR = Register(base_address, PIO_ODR_OFFSET, 0);
+   Register PIO_OSR = Register(base_address, PIO_OSR_OFFSET, 0);
    void update();
  };
